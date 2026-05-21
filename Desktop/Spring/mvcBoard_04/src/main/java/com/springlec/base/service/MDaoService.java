114b7a0f30eaf40ba5e66a5241eb98b55e8b274a
Ghost <150423973+tom3017@users.noreply.github.com>
2026-05-21 11:51:33 +0900

    Delete Desktop directory

--- Desktop/Spring/mvcBoard_04/src/main/java/com/springlec/base/service/MDaoService.java
+++
@@ -1,9 +0,0 @@
-package com.springlec.base.service;
-
-import java.util.List;
-
-import com.springlec.base.model.MDto;
-
-public interface MDaoService {
-	public List<MDto> listDao() throws Exception;
-}
